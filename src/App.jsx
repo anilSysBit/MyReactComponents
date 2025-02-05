0002e17767304eaa4f4836706f17b6b6e48eda93
anilwagle808 <anil.wagle808@gmail.com>
2025-02-05 23:13:07 +0545

    Removed aws imports

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import './app.css'
 import SliderBox from './components/ThumbnailAccordin'
 import Chatbox from './components/ChatBox'
 import BaseChatBox from './components/BaseChatBox'
-import { Buffer } from 'buffer'
 // import ImageCrop from './components/ImageCrop'
 
 
