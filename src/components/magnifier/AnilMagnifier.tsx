4b103eaa31e54dc97105719051210820e882c3be
anilwagle808 <anil.wagle808@gmail.com>
2025-02-23 13:03:30 +0545

    Created wireframes for the image magnifiers

--- src/components/magnifier/AnilMagnifier.tsx
+++ src/components/magnifier/AnilMagnifier.tsx
@@ -1,3 +1,4 @@
+import { Height, TopicRounded } from '@mui/icons-material';
 import { Hidden } from '@mui/material';
 import React, { HTMLAttributes, useState,useRef } from 'react'
 import { CSSProperties } from 'styled-components';
@@ -8,19 +9,30 @@ interface MagnifierProps{
     image?:string;
     mainContainerStyle?:CSSProperties,
     zoomLevel?:number;
+    zaliHeight?:number;
+    zaliWidth?:number;
 }
 
 const AnilMagnifier:React.FC<MagnifierProps> = ({
     image,
     zoomLevel=2,
+    zaliHeight=100,
+    zaliWidth=100,
 }) => {
     // states
     const [isZooming,setIsZooming] = useState<boolean>(false);
+    const [zaliActive,setZaliActive] = useState<boolean>(false);
+    const [[zaliX,zaliY],setZaliPosition] = useState<[number,number]>([0,0]);
     const [[mouseX,mouseY],setMousePosition] = useState<[number,number]>([0,0])
+    const [[leftWidth,topHeight],setVerticalHeight] = useState<[number,number]>([0,0]);
+    const [[rightWidth,bottomHeight],setHorizontalWidth] = useState<[number,number]>([0,0]);
     const imageContainerRef = useRef<HTMLDivElement | null>(null);
-    const lensRef = useRef<HTMLDivElement | null>(null);
+    const imageRef = useRef<HTMLDivElement | null>(null);
     const [magnifiedPosition,setMagnifiedPosition] = useState<number>(0);
     const defaultImage = 'https://picsum.photos/1000'
+
+    // const [zaliHeight,zaliWidth] = [100,150]
+
 
     const imageContainerStyle:React.CSSProperties = {
         height:'300px',
@@ -44,23 +56,62 @@ const AnilMagnifier:React.FC<MagnifierProps> = ({
         // display:'block'
     }
 
-    const mouseCenteredDistance:number = 50;
+    const MCD:number = 50; //mouse center distance
 
     const handleMouseOver =(e:React.MouseEvent)=>{
-        if(imageContainerRef.current){
-            const { left, top, width, height } = imageContainerRef.current.getBoundingClientRect();
-            const xPos = e.clientX - left;
-            const yPos = e.clientY - top;
-            setMousePosition([xPos, yPos]);
-            const imagePosition = `-${(xPos / width) * zoomLevel * 100}% -${(yPos / height) * zoomLevel * 100}%`;
-            setMagnifiedPosition(parseInt(imagePosition))
-        }
+        
         // console.log('mouse positon',e.clientX,e.clientY)
 
     }
     const handleMouseMove =(e:React.MouseEvent)=>{
-        setMousePosition([e.clientX, e.clientY]);
-        console.log('mouse positon',e.clientX,e.clientY)
+        if(imageContainerRef.current){
+            const { left, top, width, height } = imageContainerRef.current.getBoundingClientRect();
+
+            const xPos = e.clientX;
+            const yPos = e.clientY;
+            const lWidth = xPos-left
+            const yHeight = yPos-top
+            setVerticalHeight([lWidth,yHeight])
+            // console.log(left,top,width,height,xPos,yPos)
+            setMousePosition([xPos, yPos])
+
+            // zali position
+            let calcZaliX = xPos-left-(zaliWidth/2)
+            let calcZaliY = yPos-top-(zaliHeight/2)
+
+            setZaliPosition([calcZaliX,calcZaliY]);
+
+            // overlaping right
+            if(lWidth+(zaliWidth/2) >= width){
+                const overtakingX = lWidth+(zaliWidth / 2) - width
+                calcZaliX  -= overtakingX
+                // console.log('overtaking',overtakingX)
+            }
+            // overlapping bottom
+            console.log('lwidth,yHeight',lWidth,yHeight)
+            if(yHeight + (zaliHeight / 2) >= height){
+                const overtakingY = yHeight + (zaliHeight / 2) - height
+                calcZaliY -= overtakingY
+                // setZaliPosition([calcZaliX,calcZaliY - overtakingY])
+            }
+            // overlapping left
+            if(lWidth - (zaliWidth / 2) <= 0){
+                const overtakingX = lWidth - (zaliWidth / 2)
+                console.log('Yes Width less than zero',overtakingX)
+                calcZaliX -= overtakingX
+            }
+            // overlapping top
+            if(yHeight - (zaliWidth / 2) <= 0){
+                const overtakingY = yHeight - (zaliHeight / 2)
+                console.log('Yes Width less than zero',overtakingY)
+                calcZaliY -= overtakingY
+            }
+            setZaliPosition([calcZaliX,calcZaliY])
+
+
+            // const imagePosition = `-${(xPos / width) * zoomLevel * 100}% -${(yPos / height) * zoomLevel * 100}%`;
+            // setMagnifiedPosition(parseInt(imagePosition))
+        }
     }
     const handleMouseOut =(e:React.MouseEvent)=>{
 
@@ -76,16 +127,63 @@ const AnilMagnifier:React.FC<MagnifierProps> = ({
             onMouseMove={handleMouseMove}
             onMouseOut={handleMouseOut}
             >
+                <div 
+                    style={{
+                        width:leftWidth - 50,
+                        height:'100%',
+                        border:'1px solid white',
+                        position:'absolute',
+                        // backgroundColor:'rgba(0,0,0,0.6)'
+                    }}
+                    
+                    />
+                                    <div 
+                    style={{
+                        width:leftWidth + 50,
+                        height:'100%',
+                        // right:0,
+                        border:'1px solid blue',
+                        position:'absolute',
+                        // backgroundColor:'rgba(0,0,0,0.6)'
+                    }}
+                    
+                    />
+
+                <div 
+                    style={{
+                        width:'100%',
+                        height:topHeight - 50,
+                        border:'1px solid yellow',
+                        position:'absolute',
+                        // backgroundColor:'rgba(0,0,0,0.6)',
+
+                    }}
+                        />
+                                        <div 
+                    style={{
+                        width:'100%',
+                        height:topHeight +50,
+                        // bottom:0,
+                        border:'1px solid red',
+                        position:'absolute',
+                        // backgroundColor:'rgba(0,0,0,0.6)',
+
+                    }}
+                        />
         <div 
-            className="zali_filter absolute h-20 w-20 bg-slate-500"
+            className=""
             // ref={lensRef}
             style={{
-                left:mouseX - mouseCenteredDistance,
-                top:mouseY - mouseCenteredDistance
+                position:'absolute',
+                backgroundColor:'rgba(0,0,0,0.6)',
+                height:`${zaliHeight}px`,
+                width:`${zaliWidth}px`,
+                left:zaliX,
+                top:zaliY,
             }}
             >
         </div>
-            <img src={defaultImage || image} alt="" />
+            <img src={defaultImage || image} alt=""/>
         </div>
         <div 
             className='preview'
@@ -99,9 +197,7 @@ const AnilMagnifier:React.FC<MagnifierProps> = ({
                     // position:'absolute',
                     left:0,
                     top:0,
-                    height:'1200px',
-                    width:'1800px',
-                    transform:`translate(${mouseX * 5}px,${-mouseY * 2}px)`,
+                    transform:`translate(${mouseX }px,${mouseY }px)`,
                     
                     // width:'auto',
                     // visibility:'hidden',
